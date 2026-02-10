5d1702d9e493a0fb4fbf6d982afcae3f49e43907
Nikolay-Sazonov <koly-sazon@yandex.ru>
2026-02-10 13:15:34 +0800

    corrected version

--- skript.js
+++ skript.js
@@ -1,42 +1,42 @@
 // лекция
-const recolorTheFirstCardButton = document.getElementById('recolor-the-first-card-button')
-const firstProductCard = document.querySelector('.product-card')
-const greenHashColor = '#00ff0d'
+const theFirstCardRecolorButton = document.getElementById('recolor-the-first-card-button');
+const firstProductCard = document.querySelector('.product-card');
+const greenHashColor = '#00ff0d';
 
-recolorTheFirstCardButton.addEventListener('click', ()=>  {
+theFirstCardRecolorButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = greenHashColor;
-})
+});
 
-const productCards = document.querySelectorAll('.product-card')
-const recolorTheAllCardButton = document.getElementById('recolor-the-all-card-button')
-const yellowHashColor = '#e2d520'
+const productCards = document.querySelectorAll('.product-card');
+const theAllCardRecolorButton = document.getElementById('recolor-the-all-card-button');
+const yellowHashColor = '#e2d520';
 
-recolorTheAllCardButton.addEventListener('click', ()=> {
-  productCards.forEach(
-    card => card.style.backgroundColor = yellowHashColor
-  );
-})
+theAllCardRecolorButton.addEventListener('click', () => {
+  productCards.forEach(card => {
+    card.style.backgroundColor = yellowHashColor;
+  });
+});
 
-const openGoogleButton = document.getElementById('open-google-button')
-openGoogleButton.addEventListener('click', openGoogle);
+const googleOpenButton = document.getElementById('open-google-button');
+googleOpenButton.addEventListener('click', openGoogle);
 
-function openGoogle(){
-  const answer = confirm ('Вы действительно хотите перейти на сайт Google')
-  if (answer == true) {
-    window.open('https://www.google.com/')
+function openGoogle() {
+  const answer = confirm ('Вы действительно хотите перейти на сайт Google');
+  if (answer) {
+    window.open('https://www.google.com/');
   }else {
-    console.log('переход отменен')
+    console.log('переход отменен');
   }
-}
+};
 
 // Домашнее задание
-const textTitle= document.querySelector('.product-card__title')
+const textTitle = document.querySelector('.product-card__title');
   textTitle.addEventListener('mouseover', ()=>{
     console.log('было непросто');
-  })
+  });
 
-const recolorButton = document.getElementById('recolor-button')
+const recolorButton = document.getElementById('recolor-button');
 
 recolorButton.addEventListener('click',  () => {
   recolorButton.classList.toggle('active');
-})
+});
