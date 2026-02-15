881ab9891a388588853ec3cacddcf6aafa4300b7
Nikolay-Sazonov <koly-sazon@yandex.ru>
2026-02-15 20:37:35 +0800

    correction of comments

--- homework-5.js
+++ homework-5.js
@@ -1,56 +1,60 @@
 //  Задание 3: Создание функции "температура воздуха в Иркутске".
-const show = (city, temperature) => {
-console.log (`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`)
+const readingTemperature = (city, temperature) => {
+console.log (`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 };
 
-show('Иркутск', '-25');
+readingTemperature('Иркутск', '-25');
 
 // Задание 4: Создание функции сравнения.
 const LIGHT_SPEED = 299792;
 const speed = 2919222;
 
-if (LIGHT_SPEED > speed){
-  console.log('Субсветовая скорость')
-} else if (LIGHT_SPEED === speed){
-  console.log('Скорость света')
-}else{
-  console.log('Сверхсветовая скорость')
+const comparisonSpeed = () => {
+  if (LIGHT_SPEED > speed) {
+    console.log('Субсветовая скорость');
+  } else if (LIGHT_SPEED === speed) {
+    console.log('Скорость света');
+  } else {
+    console.log('Сверхсветовая скорость');
+  }
 };
+
+comparisonSpeed ();
 
 // Задание 5: Создание функции подсчета.
 const product = 'Велосипед';
 let price = 68346;
 
-const purchase = (finance ) => {
- if (finance >= price){
-  console.log(`${product} приобретён. Спасибо за покупку!`)
- }else{
-   console.log(`Вам не хватает ${price - finance}. Пополните баланс.`)
- }
-}
+const buyProduct = (finance ) => {
+  if (finance >= price) {
+    console.log(`${product} приобретён. Спасибо за покупку!`);
+  } else {
+    console.log(`Вам не хватает ${price - finance}. Пополните баланс.`);
+  }
+};
 
-purchase(700000);
+buyProduct(700000);
 
 // Задание 6: Создание своей функции.
 let bank = 900;
 
-const shopping = (milk = 100, shawarma = 50, juice = 300) => {
-  if (bank > milk + shawarma + juice){
-    console.log(`Ваша сдача составляет ${bank-(milk + shawarma + juice)} рублей. Спасибо за покупку!`)
-  } else if (bank === milk + shawarma + juice){
-    console.log('Спасибо за покупку')
-  }else{
-    console.log(`Недостаточно средств в размере ${(milk + shawarma + juice)- bank}. Необходимо пополнить баланс `)
+const shoppingGrocery = (milk = 100, shawarma = 50, juice = 300) => {
+  if (bank > milk + shawarma + juice) {
+    console.log(`Ваша сдача составляет ${bank-(milk + shawarma + juice)} рублей. Спасибо за покупку!`);
+  } else if (bank === milk + shawarma + juice) {
+    console.log('Спасибо за покупку');
+  } else {
+    console.log(`Недостаточно средств в размере ${(milk + shawarma + juice)- bank}. Необходимо пополнить баланс `);
   }
-}
+};
 
-shopping();
+shoppingGrocery();
 
 // Задание 7: Создание переменной.
 
 let footballClub = "CSKA";
 const G = 9.81; 
-let climate = "cold"
+let climate = "cold";
 
 console.log(footballClub);
 console.log(G);
