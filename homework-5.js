822910acff52b012e23b731b519c65a8e3cae71c
Nikolay-Sazonov <koly-sazon@yandex.ru>
2026-02-23 11:01:35 +0800

    fixing array errors

--- homework-5.js
+++ homework-5.js
@@ -1,60 +1,60 @@
 // //  Задание 3: Создание функции "температура воздуха в Иркутске".
-// const showTemperature = (city, temperature) => {
-// console.log (`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
-// };
+const showTemperature = (city, temperature) => {
+console.log (`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
+};
 
-// showTemperature('Иркутск', '-25');
+showTemperature('Иркутск', '-25');
 
 // // Задание 4: Создание функции сравнения.
-// const LIGHT_SPEED = 299792;
 
-// const comparisonSpeed = (speed) => {
-//   if (LIGHT_SPEED > speed) {
-//     console.log('Субсветовая скорость');
-//   } else if (LIGHT_SPEED === speed) {
-//     console.log('Скорость света');
-//   } else {
-//     console.log('Сверхсветовая скорость');
-//   }
-// };
+const LIGHT_SPEED = 299792;
 
-// comparisonSpeed (2919222);
+const comparisonSpeed = (speed) => {
+  if (LIGHT_SPEED > speed) {
+    console.log('Субсветовая скорость');
+  } else if (LIGHT_SPEED === speed) {
+    console.log('Скорость света');
+  } else {
+    console.log('Сверхсветовая скорость');
+  }
+};
+
+comparisonSpeed (2919222);
 
 // // Задание 5: Создание функции подсчета.
-// const product = 'Велосипед';
-// let price = 68346;
 
-// const buyProduct = (finance ) => {
-//   if (finance >= price) {
-//     console.log(`${product} приобретён. Спасибо за покупку!`);
-//   } else {
-//     console.log(`Вам не хватает ${price - finance}. Пополните баланс.`);
-//   }
-// };
+const product = 'Велосипед';
+let price = 68346;
 
-// buyProduct(700000);
+const buyProduct = (finance ) => {
+  if (finance >= price) {
+    console.log(`${product} приобретён. Спасибо за покупку!`);
+  } else {
+    console.log(`Вам не хватает ${price - finance}. Пополните баланс.`);
+  }
+};
+
+buyProduct(700000);
 
 // // Задание 6: Создание своей функции.
-// let bank = 900;
 
-// const shoppingGrocery = (milk = 100, shawarma = 50, juice = 300) => {
-//   if (bank > milk + shawarma + juice) {
-//     console.log(`Ваша сдача составляет ${bank-(milk + shawarma + juice)} рублей. Спасибо за покупку!`);
-//   } else if (bank === milk + shawarma + juice) {
-//     console.log('Спасибо за покупку');
-//   } else {
-//     console.log(`Недостаточно средств в размере ${(milk + shawarma + juice)- bank}. Необходимо пополнить баланс `);
-//   }
-// };
+let bank = 900;
 
-// shoppingGrocery();
+const shoppingGrocery = (milk = 100, shawarma = 50, juice = 300) => {
+  if (bank > milk + shawarma + juice) {
+    console.log(`Ваша сдача составляет ${bank-(milk + shawarma + juice)} рублей. Спасибо за покупку!`);
+  } else if (bank === milk + shawarma + juice) {
+    console.log('Спасибо за покупку');
+  } else {
+    console.log(`Недостаточно средств в размере ${(milk + shawarma + juice)- bank}. Необходимо пополнить баланс `);
+  }
+};
+
+shoppingGrocery();
 
 // // Задание 7: Создание переменной.
 
-// let footballClub = "CSKA";
-// const G = 9.81; 
-// let climate = "cold";
+let footballClub = "CSKA";
+const G = 9.81; 
+let climate = "cold";
 
-// console.log(footballClub);
-// console.log(G);
-// console.log(climate);
--- homework-6.js
+++ homework-6.js
@@ -15,7 +15,7 @@ console.log(personalData);
 
 // Задание 4: Создание объекта с персональными данными..
 
-const infomationCar = {
+const car = {
   brand: "Jetour",
   model:"Dashing",
   colore:"blue",
@@ -23,26 +23,26 @@ const infomationCar = {
   transmission:"robot"
 };
 
-infomationCar.owner = personalData;
-console.log(infomationCar,);
+car.owner = personalData;
+console.log(car);
 
 // Задание 5: Создание функции поиска "максимальной скорости" в объекте infomationCar.
 
-const searcSpeed = (infomationCar) => {
-  if ("maxSpeed" in infomationCar) {
+const setMaxSpeed = (car) => {
+  if ("maxSpeed" in car) {
   } else {
-    infomationCar.maxSpeed = "200 км/ч";
+    car.maxSpeed = "200 км/ч";
   }
 };
-searcSpeed(infomationCar);
+setMaxSpeed(car);
 
 // Задание 6:  Создание функции, которая получает первым аргументом  — объект, а вторым аргументом — свойство объекта.
 
-const showInfo = (objeсt, key) => {
+const showObjectProperty = (objeсt, key) => {
   console.log(objeсt[key]);
 };
 
-showInfo (personalData, 'city');
+showObjectProperty (personalData, 'city');
 
 // Задание 7: Cоздание массива.
 
@@ -52,28 +52,71 @@ console.log(productList);
 
 // Задание 8: Cоздание массива состоящий из объектов.
 
-const books = [
-  { name : "Выйди из зоный комфорта. Измени жизнь",coverColor : "голубая", nameAuthor : "Брайаном Трейси", produced : "2001", genre : "психологии" },
-  { name : "Сила воли. Как развить и укрепить", coverColor : "белая", nameAuthor : "Келли Макгонигал", produced : "2012", genre : "психологии, личностный рост" },
-  { name : "Десять негритят", coverColor : "черная", nameAuthor : "Агата Кристи", produced : "1939", genre : "детектив" }
+const favoriteBooks = [
+  {
+    name : "Выйди из зоный комфорта. Измени жизнь",
+    coverColor : "голубая",
+    nameAuthor : "Брайаном Трейси",
+    produced : "2001",
+    genre : "психологии"
+  },
+  {
+    name : "Сила воли. Как развить и укрепить",
+    coverColor : "белая",
+    nameAuthor : "Келли Макгонигал",
+    produced : "2012",
+    genre : "психологии, личностный рост"
+  },
+  {
+    name : "Десять негритят",
+    coverColor : "черная",
+    nameAuthor : "Агата Кристи",
+    produced : "1939",
+    genre : "детектив"
+  }
 ];
 
-books.push ({ name : "Девушка с татуировкой дракона", coverColor : "черная", nameAuthor : "Стиг Ларссон ", produced : "1939", genre : "детектив" });
-console.log(books);
+favoriteBooks.push (
+  {
+    name : "Девушка с татуировкой дракона",
+    coverColor : "черная",
+    nameAuthor : "Стиг Ларссон",
+    produced : "1939",
+    genre : "детектив"
+  }
+);
+console.log(favoriteBooks);
 
 // Задание 9: Cоздание массива состоящий из объектов и объединение с массивом из задания №8.
 
 const booksMarvel = [
-  { name : "Человек-Паук. Веном-фактор",coverColor : "серая", nameAuthor : "Диана Дуэйн", produced : "1990", genre : "Роман, супергероика" },
-  { name : "Доктор Стрэндж. Участь снов", coverColor : "черная", nameAuthor : "Девин Грейсон", produced : "2017", genre : "Фэнтези, проза" },
-  { name : "Дэдпул. Лапы", coverColor : "черно-красная", nameAuthor : "Стефан Петручо", produced : "2016", genre : " Современная зарубежная проза, супергероика, юмор" }
+  {
+    name : "Человек-Паук. Веном-фактор",
+    coverColor : "серая",
+    nameAuthor : "Диана Дуэйн",
+    produced : "1990",
+    genre : "Роман, супергероика"
+  },
+  {
+    name : "Доктор Стрэндж. Участь снов",
+    coverColor : "черная",
+    nameAuthor : "Девин Грейсон",
+    produced : "2017",
+    genre : "Фэнтези, проза"
+  },
+  {
+    name : "Дэдпул. Лапы",
+    coverColor : "черно-красная",
+    nameAuthor : "Стефан Петручо",
+    produced : "2016",
+    genre : " Современная зарубежная проза, супергероика, юмор"
+  }
 ];
- const allBooks = [...books, ...booksMarvel];
+ const allBooks = [...favoriteBooks, ...booksMarvel];
  
 console.log(allBooks);
 
 // Задание 10: Создать функцию, которая принимает массив сущностей
-
 
 const getRerityInfomation = allBooks.map( book => {
   if (book.produced <= 2001) {
@@ -84,5 +127,3 @@ const getRerityInfomation = allBooks.map( book => {
 });
 
 console.log(getRerityInfomation);
-
-
