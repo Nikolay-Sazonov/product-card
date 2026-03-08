702ce74346a85eee390969c9c35e0ddbb568c3af
Nikolay-Sazonov <koly-sazon@yandex.ru>
2026-03-08 16:16:05 +0800

    fix error

--- homework-7.js
+++ homework-7.js
@@ -1,10 +1,10 @@
 // Уровень 1.
-
+import { commentsSocialMedia } from "./comments.js";
 // Задание 2. Создание массива чисел от 1 до 10 и фильтрация его от значения 5.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const filtrationNumbers = numbers.filter(number => 
-  number >=5 
+  number >=5,
 );
 
 // Задание 3. Создать массив строк и воспользоваться методом .includes
@@ -19,7 +19,7 @@ const carparts = [
   'кардан',
   'редуктор',
   'подшипник',
-  'помпа'
+  'помпа',
 ];
 
 const selectionCarparts = carparts.includes('генератор');
@@ -27,50 +27,56 @@ const selectionCarparts = carparts.includes('генератор');
 //Задание 3. Создать массив строк и воспользоваться методом .reverse
 
 const getInvertedArray = (array) => {
-  console.log(array.reverse())
+  array.reverse();
 };
 
+getInvertedArray(carparts);
 
 // Уровень 2
 
 // Заданиие 7. Вывести в консоль массив тех комментариев, почта пользователей которых содержит ".com"
-import { commentsSocialMedia } from "./comments.js";
-const chooseCommentsSocialMedia = commentsSocialMedia.filter(comment =>
-  comment.email.includes('.com')
+
+const getCommentsSocialMedia = commentsSocialMedia.filter(comment =>
+  comment.email.includes('.com'),
 );
 
 // Задание 8. Перебрать массив
 
-const newCommentsSocialMedia = commentsSocialMedia.map(comment => {
-  return {...comment, postId: comment.id <= 5 ? 2 : 1 }
+const getNewCommentsSocialMedia = commentsSocialMedia.map(comment => {
+  return {
+    ...comment,
+    postId: comment.id <= 5 ? 2 : 1
+  };
 });
 
 // Задание 9. Перебрать массив, что бы объекты состояли только из айди и имени
 
-const newArray = commentsSocialMedia.map(comment => ({
+const getNewArray = commentsSocialMedia.map(comment => ({
     id : comment.id,
-    name : comment.name
+    name : comment.name,
 }));
 
 // Задание 10. Перебираем массив, добавляем объектам свойство isInvalid
 
-const newComments = commentsSocialMedia.map(comment => ({
+const getNewComments = commentsSocialMedia.map(comment => ({
   ...comment,
-  isInvalid : comment.body.length > 180
-}));
+  isInvalid : comment.body.length > 180,
+})
+);
 
 
 // Задание 11. Используя метод reduce и map вывести массив почты.
 
-const arrayMail = commentsSocialMedia.map( comment => 
-comment.email
-  );
+const getArrayMail = commentsSocialMedia.map( comment => 
+  comment.email,
+);
 
-const newArrayMail = commentsSocialMedia.reduce((acc, comment) => {
+const getNewArrayMail = commentsSocialMedia.reduce((acc, comment) => {
   acc.push(comment.email);
   return acc;
-}, []);
+},
+[]);
 
 // Задание 12. Перебрав массив с задания №11, привести его к строке.
 
-const newArrayMailStr = newArrayMail.join();
+const getNewArrayMailStr = newArrayMail.join();
